7cf4153a824d0f4e0781dd169b90426bce9801e1
Jim Rippee <jim.rippee@gmail.com>
2021-08-11 20:12:45 -0700

    add sec group

--- main.tf
+++ main.tf
@@ -3,12 +3,28 @@ provider "aws" {
   
 }
 resource "aws_instance" "example" {
-    ami = "ami-0d382e80be7ffdae5"
-    instance_type = "t2.nano"
+    ami                     = "ami-0d382e80be7ffdae5"
+    instance_type           = "t2.nano"
+    vpc_security_group_ids  = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World. Love you Nina" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
 
     tags = {
       "Name" = "terraform-example"
-    }   
-  
+    }
 }
+resource "aws_security_group" "instance" {
+  name = "teraform-example-instance"
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
